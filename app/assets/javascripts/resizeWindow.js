60ba73566e707408ef1b17778fa1ae6263663449
Ian Leggett <ileggett@equalexperts.com>
2017-03-07 16:58:10 +0000

    All iframe'd patterns now have buttons that can resize the iframe widow to demonstrate their responsive nature

--- app/assets/javascripts/resizeWindow.js
+++ app/assets/javascripts/resizeWindow.js
@@ -1,6 +1,6 @@
 function resizeWindow(el, size){
 
-  var iframeExample = $(el).parent().find('.hack_example')
+  var iframeExample = $(el).parent().parent().find('.hack_example')
 
   $(iframeExample).removeClass();
   $(iframeExample).addClass('hack_example ' + size);
