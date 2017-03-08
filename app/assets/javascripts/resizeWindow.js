f8faf4b10e9f17024852c8108e0234542aa160fe
Ian Leggett <ileggett@equalexperts.com>
2017-03-08 12:27:53 +0000

    Updated jQuery code a result of a PR review

--- app/assets/javascripts/resizeWindow.js
+++
@@ -1,7 +0,0 @@
-function resizeWindow(el, size){
-
-  var iframeExample = $(el).parent().parent().find('.hack_example')
-
-  $(iframeExample).removeClass();
-  $(iframeExample).addClass('hack_example ' + size);
-}
